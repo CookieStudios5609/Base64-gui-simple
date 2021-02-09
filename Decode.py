54fba15c12f8af405b44ea680ae7433418af1432
2600 <irhstuffz@gmail.com>
2021-02-08 23:12:37 -0500

    Replaces strings in decode.py and encode.py

--- Decode.py
+++ Decode.py
@@ -1,6 +1,6 @@
 import base64
 
-encoded = 'c3V1dXVwcHBwb3I=='  # encoded here, very cool
+encoded = 'WW91ciBUZXh0IEhlcmU='  # encoded here, very cool
 base64_bytes = encoded.encode('ascii')
 message_bytes = base64.b64decode(base64_bytes)
 decoded = message_bytes.decode('ascii')
--- Encode.py
+++ Encode.py
@@ -1,6 +1,6 @@
 import base64
 
-inputmsg = 'wow'
+inputmsg = 'Your Text Here'
 message_bytes = inputmsg.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 inputenc = base64_bytes.decode('ascii')
